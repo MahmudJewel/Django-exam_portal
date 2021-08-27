5e933b958fcf1b441f8db715400f252113af8cdd
villain <villain@kali.local>
2021-08-28 00:51:46 +0600

    one login page is added

--- online_exam/home/urls.py
+++ online_exam/home/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LogoutView,LoginView
+from django.contrib.auth.views import LogoutView, LoginView
 from home import views
 
 urlpatterns = [
     path('', views.home),
     path('afterlogin', views.afterlogin_view,name='afterlogin'),
-
+    path('login', LoginView.as_view(template_name='home/login.html'),name='login'),
     path('logout', LogoutView.as_view(template_name='home/logout.html'),name='logout'),
 ]  
